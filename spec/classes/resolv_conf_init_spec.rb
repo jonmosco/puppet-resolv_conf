719e050cc6f5ca080f88012f2454434a9ac7fe8b
Jon Mosco <jonny.mosco@gmail.com>
2014-10-25 18:36:40 -0400

    added ci to rakefile

--- spec/classes/resolv_conf_init_spec.rb
+++ spec/classes/resolv_conf_init_spec.rb
@@ -3,12 +3,12 @@ require 'spec_helper'
 describe 'resolv_conf' do
 
   it do
-    should contain_file('resolv.conf').with(
+    should contain_file('resolv.conf').with({
       'ensure' => 'file',
       'owner'  => 0,
       'group'  => 0,
       'mode'   => '0644',
-    )
+    })
   end
 
 end
