39c54cc939fa619439b2b157c2a812bf282b398b
Jon Mosco <jonny.mosco@gmail.com>
2014-10-29 00:28:22 -0400

    added more testing for parameters

--- spec/classes/resolv_conf_init_spec.rb
+++ spec/classes/resolv_conf_init_spec.rb
@@ -11,4 +11,20 @@ describe 'resolv_conf' do
     })
   end
 
+  context "with nameserver => 127.0.0.1" do
+    let(:params) {{
+      :nameserver => '127.0.0.1',
+    }}
+
+    context "with domain => localdomain" do
+      let(:params) {{
+        :domain => 'localdomain',
+      }}
+
+      it do
+        should contain_file('resolv.conf').with \
+          .with_content(/# resolv.conf: Managed by puppet\.\n#\ndomain localdomain\nnameserver 127.0.0.1\n/)
+      end
+    end
+  end
 end
